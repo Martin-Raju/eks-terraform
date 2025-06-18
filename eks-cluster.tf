8eaddf881205ac57d766e3cd95d7cd92aa0e04eb
martin Raju <martin.raju313@gmail.com>
2025-06-18 18:40:48 +0530

    updated

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -13,6 +13,9 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
     instance_types         = ["t3.medium"]
